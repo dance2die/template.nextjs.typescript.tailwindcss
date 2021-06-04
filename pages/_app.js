f98630348048744fc290be44e01efebf7963026d
dance2die <sungmkim80@gmail.com>
2021-06-04 15:35:29 -0400

    Added strict mode on _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { StrictMode } from 'react';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<StrictMode>
+			<Component {...pageProps} />
+		</StrictMode>
+	);
 }
 
 export default MyApp;
